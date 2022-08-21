6ee40fa6d7a61cf3b04253410d38d5d3623b54d6
Tuyifan0401 <110735403+Tuyifan0401@users.noreply.github.com>
2022-08-21 10:42:50 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -225,9 +225,9 @@ def main(username, password, cookie):
         res = hit_carder.post()
         print(res)
         if str(res['e']) == '0':
-            return 0, '徒徒打卡成功'
+            return 0, '打卡成功'
         elif str(res['m']) == '今天已经填报了':
-            return 0, '徒徒今天已经打卡'
+            return 0, '今天已经打卡'
         else:
             return 1, '打卡失败'
     except:
